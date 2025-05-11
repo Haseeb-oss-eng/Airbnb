bcfa4cee7b72f7f34bfc52ce0093c1a84eee2436
Haseeb <ibnhabeebulla@gmail.com>
2025-05-11 18:33:00 +0530

    insert

--- home.py
+++ home.py
@@ -1,3 +1,14 @@
 import streamlit as st
+from pages import Info, details
 
 st.sidebar.title("Airbnb App")
+option = st.sidebar.radio("Tab",["About Us", "Listing Properties", "Customized Information"])
+
+
+if option == "About Us":
+    st.title("Airbnb")
+
+elif option == "Listing Properties":
+    Info.app()
+elif option == "Customized Information":
+    details.app()
